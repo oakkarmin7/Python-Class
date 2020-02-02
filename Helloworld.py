090d9765dcf275875467a1cd2ffc2934e4073b42
Oakkarmin7 <cfcoakkar@gmail.com>
2020-02-02 11:04:31 +0630

    First commit

---
+++ Helloworld.py
@@ -0,0 +1,28 @@
+username - brainseeddeveloper
+email - brainseed.developer@gmail.com
+passward -*@(#*)#)@
+
+step 1 : Git Sown;oad
+step 2 : Git Tntallation
+step 3 : Github Account Creation
+step 4 : Git Repository
+step 5 : A File To upload on github
+step 6 : Git Command Line
+ cd Path - Go to Directory
+
+Firt Time ( New Repository )
+	    git init
+	    git add  .
+	    git config --global user.email
+	    "useremail"
+	    git config --global user.name "username"
+
+	    git comm=mit -m "first commit"
+	    git remote add origin https://github.com/brainseeddeveloper/Sample.git
+	    git push -u orgin master
+
+(New Repository For New Project )
+	    git init
+	    git add .
+	    git commit -m "first commit"
+	    git remote add origin https://github.com/brainseeddeveloper
