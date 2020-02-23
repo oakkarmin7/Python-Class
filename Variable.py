ce947050baea26da32d172e79806721136197d8a
Oakkarmin7 <cfcoakkar@gmail.com>
2020-02-23 12:46:20 +0630

    Variable % List

---
+++ Variable.py
@@ -0,0 +1,64 @@
+#Variable.py
+
+>>> 2 + 2
+4
+>>> 50 - 5 * 6
+20
+>>> (50 - 5 * 6) / 4
+5.0
+>>> round(5.0)
+5
+>>> 5.123456
+5.123456
+>>> round (5.123456,2)
+5.12
+>>> 17 / 3
+
+* / % + - (operator)
+
+ addition = 75 + 25
+ subtraction = 204 - 204
+ multipication = 100 * 0.5
+ division = 90 / 9
+ power of 2 = 3 ** 2
+ the remainder of the division = 10 % 3
+
+ Variable
+
+int - integer - 1,2,3, ----> 308930 etc
+float -		  - 1.0, 5.9, 6.0, 63.23, etc
+string -	  - 'Hello', "World" , "A" ,"B",
+
+a = 1
+a (Variable) = (assign) 1 (value)
+
+width = 20
+height = 5 * 9
+vol = width * height
+vol
+
+sale = 1500
+tax = 5 / 100
+total_tax = sale * taxtotal_tax
+total price = sale +total_tax
+total_price
+
+
+print('spam eggs')
+print(' don\'t ')
+print("dont't")
+print('"Yes"')
+print("\"Yes,\" they said.")
+print('"Isn\'t," they said')
+
+print('"In\'t, they said"')
+
+s = 'First Line.\nSecond Line.'
+print(s)
+
+print("""\
+Usage: thingy
+		-a
+		-b
+		-c
+""")
